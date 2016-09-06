b3050d79ecdf8d01b6a6f5bd90572fe0c01703e7
Eclerr <eclerrrr@yandex.ru>
2016-09-06 17:34:41 +0300

    add build section about

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
   
+
+	var slider = new Flickity(".slider", {
+		wrapAround: true,
+		autoPlay: 3000,
+	});
+
+
+	// TEST
+	$("a").on("click",function(e){
+		e.preventDefault();
+	})
+
 });
